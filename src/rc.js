f7e448c49248417bbd52bc6ed61a8eef333922eb
Brandon Plaster <brandon@littlstar.com>
2018-10-26 12:54:12 -0400

    refactor(rc): use data.root

--- src/rc.js
+++ src/rc.js
@@ -1,18 +1,16 @@
 const rc = require('ara-runtime-configuration')
 const { resolve } = require('path')
 const extend = require('extend')
-const os = require('os')
 
-const ARA_DIR = '.ara'
 const DCDN_DIR = 'dcdn'
 const DEFAULT_CONFIG_STORE = 'store.json'
 const DEFAULT_JOB_STORE = 'jobs.json'
 
 const defaults = () => ({
   dcdn: {
-    root: resolve(os.homedir(), ARA_DIR, DCDN_DIR),
-    config: resolve(os.homedir(), ARA_DIR, DCDN_DIR, DEFAULT_CONFIG_STORE),
-    jobs: resolve(os.homedir(), ARA_DIR, DCDN_DIR, DEFAULT_JOB_STORE)
+    root: resolve(rc().data.root, DCDN_DIR),
+    config: resolve(rc().data.root, DCDN_DIR, DEFAULT_CONFIG_STORE),
+    jobs: resolve(rc().data.root, DCDN_DIR, DEFAULT_JOB_STORE)
   }
 })
 
