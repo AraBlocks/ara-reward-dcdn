ee02a745ae367791a09a1648223946945f06856e
hdao1121 <huy@littlstar.com>
2018-11-01 15:13:15 -0400

    feat(): add signing and verifying utilities

--- src/util.js
+++ src/util.js
@@ -1,3 +1,35 @@
+const crypto = require('ara-crypto')
+const rc = require('ara-runtime-configuration')()
+const { resolve } = require('path')
+const { DID } = require('did-uri')
+const pify = require('pify')
+const { readFile } = require('fs')
+const ss = require('ara-secret-storage')
+
+
+async function sign(user) {
+  const did = new DID(user.did)
+  const password = crypto.blake2b(Buffer.from(user.pass))
+  const publicKey = Buffer.from(did.identifier, 'hex')
+  const hash = crypto.blake2b(publicKey).toString('hex')
+  const path = resolve(rc.network.identity.root, hash, 'keystore/ara')
+  const keystore = JSON.parse(await pify(readFile)(path, 'utf8'))
+  const secretKey = ss.decrypt(keystore, { key: password.slice(0, 16) })
+  const message = Buffer.from(user.did)
+  return crypto.sign(message, secretKey)
+}
+
+function verify(signature) {
+  const signatureData = Buffer.from(signature.getData())
+  const signatureDid = 'did:ara:' + signature.getAraId().getDid()
+
+  const did = new DID(signatureDid)
+  const publicKey = Buffer.from(did.identifier, 'hex')
+  const message = Buffer.from(signatureDid)
+
+  return crypto.verify(signatureData, message, publicKey)
+}
+
 class Countdown {
   constructor(count, onComplete) {
     this.count = count
@@ -13,5 +45,7 @@ class Countdown {
 }
 
 module.exports = {
-  Countdown
+  Countdown,
+  sign,
+  verify
 }
