4bcbc5b8dfb06c5f5eef209a0918d8424193ae99
Brandon Plaster <brandon.a.plaster@gmail.com>
2018-12-01 00:40:02 -0500

    fix(requester): only try to send rewards once

--- test/requester.js
+++ test/requester.js
@@ -5,7 +5,6 @@ const {
 const { Countdown } = require('../src/util')
 const { Requester } = require('../src/requester')
 const User = require('../src/user')
-
 const {
   Agreement,
   Quote,
@@ -15,29 +14,64 @@ const { token: { expandTokenValue } } = require('ara-contracts')
 const { rewards } = require('ara-contracts')
 const sinon = require('sinon')
 const test = require('ava')
+const EventEmitter = require('events')
 
-const user = new User('did', 'pass')
+const timeout = ms => new Promise(res => setTimeout(res, ms))
 
-const jobNonce = Buffer.from('did', 'hex')
+const TEST_USER = new User('did', 'pass')
+const TEST_JOB_NONCE = Buffer.from('did', 'hex')
+const TEST_STREAM = { on: () => true }
+const TEST_AFS = { 
+  discoveryKey: 'key', 
+  replicate: () => TEST_STREAM, 
+  partitions:  {
+    home: {
+      content: {
+        on: () => true
+      }
+    }
+  }
+}
 
-const stream = { on: () => true }
-const afs = { discoveryKey: 'key', replicate: () => stream }
+const TEST_PRICE = Number(expandTokenValue('10'))
+const TEST_MATCHER = new matchers.MaxCostMatcher(TEST_PRICE, 3)
+const TEST_SWARM = {}
+const TEST_QUEUE = { async push() { return true } }
 
-const convertedPrice = Number(expandTokenValue('10'))
-const matcher = new matchers.MaxCostMatcher(convertedPrice, 3)
-const swarm = {}
-const queue = { async push() { return true } }
+const TEST_REQ_SIGNATURE = new Signature()
+sinon.stub(TEST_REQ_SIGNATURE, 'getData').returns('data')
+sinon.stub(TEST_REQ_SIGNATURE, 'getDid').returns('id0')
+sinon.stub(TEST_USER, 'sign').returns(TEST_REQ_SIGNATURE)
+sinon.stub(TEST_USER, 'verify').returns(true)
 
-sinon.stub(Requester.prototype, '_download')
+test('requester.download', async (t) => {
+  const afs = { 
+    discoveryKey: 'key', 
+    replicate: () => stream, 
+    partitions:  {
+      home: {
+        content: new EventEmitter(),
+      }
+    }
+  }
+  afs.partitions.home.content.peers = []
 
-const requesterSignature = new Signature()
-sinon.stub(requesterSignature, 'getData').returns('data')
-sinon.stub(requesterSignature, 'getDid').returns('id0')
-sinon.stub(user, 'sign').returns(requesterSignature)
-sinon.stub(user, 'verify').returns(true)
+  const sendRewardsFake = sinon.fake()
+  const requester = new Requester(TEST_JOB_NONCE, TEST_MATCHER, TEST_USER, afs, TEST_SWARM, TEST_QUEUE)
+  sinon.stub(requester, '_sendRewards').callsFake(sendRewardsFake)
+
+  requester._download()
+  afs.partitions.home.content.emit('sync')
+  afs.partitions.home.content.emit('sync')
+  afs.partitions.home.content.emit('sync')
+  afs.partitions.home.content.emit('sync')
+
+  await timeout(1000)
+  t.true(1 === sendRewardsFake.callCount)
+})
 
 test('requester.validateQuote', async (t) => {
-  const requester = new Requester(jobNonce, matcher, user, afs, swarm, queue)
+  const requester = new Requester(TEST_JOB_NONCE, TEST_MATCHER, TEST_USER, TEST_AFS, TEST_SWARM, TEST_QUEUE)
   const quote = new Quote()
   const validation = await requester.validateQuote(quote)
   t.true(validation)
@@ -45,16 +79,16 @@ test('requester.validateQuote', async (t) => {
 
 test('requester.generateAgreement', async (t) => {
   const quote = new Quote()
-  const requester = new Requester(jobNonce, matcher, user, afs, swarm, queue)
+  const requester = new Requester(TEST_JOB_NONCE, TEST_MATCHER, TEST_USER, TEST_AFS, TEST_SWARM, TEST_QUEUE)
   const agreement = await requester.generateAgreement(quote)
-  t.true(agreement.getSignature() == requesterSignature && agreement.getQuote() == quote)
+  t.true(agreement.getSignature() == TEST_REQ_SIGNATURE && agreement.getQuote() == quote)
 })
 
 test('requester.validateAgreement', async (t) => {
   const agreementNonce = Buffer.from('did', 'hex')
   const agreement = new Agreement()
   agreement.setNonce(agreementNonce)
-  const requester = new Requester(jobNonce, matcher, user, afs, swarm, queue)
+  const requester = new Requester(TEST_JOB_NONCE, TEST_MATCHER, TEST_USER, TEST_AFS, TEST_SWARM, TEST_QUEUE)
 
   const validation = await requester.validateAgreement(agreement)
   t.true(validation)
@@ -65,7 +99,7 @@ test('requester.generateReward', async (t) => {
   const units = 5
   const total = units * cost
 
-  const requester = new Requester(jobNonce, matcher, user, afs, swarm, queue)
+  const requester = new Requester(TEST_JOB_NONCE, TEST_MATCHER, TEST_USER, TEST_AFS, TEST_SWARM, TEST_QUEUE)
 
   const agreement = new Agreement()
   const quote = new Quote()
@@ -73,13 +107,13 @@ test('requester.generateReward', async (t) => {
   agreement.setQuote(quote)
 
   const reward = requester.generateReward(agreement, units)
-  t.true(reward.getSignature() == requesterSignature && total == reward.getAmount() && reward.getAgreement() == agreement)
+  t.true(reward.getSignature() == TEST_REQ_SIGNATURE && total == reward.getAmount() && reward.getAgreement() == agreement)
 })
 
 test('requester.dataReceived', async (t) => {
   const id1 = 'id1'
   const id2 = 'id2'
-  const requester = new Requester(jobNonce, matcher, user, afs, swarm, queue)
+  const requester = new Requester(TEST_JOB_NONCE, TEST_MATCHER, TEST_USER, TEST_AFS, TEST_SWARM, TEST_QUEUE)
 
   const deliveryMap = new Map()
   deliveryMap.set(id1, 5)
@@ -93,7 +127,7 @@ test('requester.dataReceived', async (t) => {
 
 test('requester.stop', async (t) => {
   const leaveFake = sinon.fake()
-  const requester = new Requester(jobNonce, matcher, user, afs, swarm, queue)
+  const requester = new Requester(TEST_JOB_NONCE, TEST_MATCHER, TEST_USER, TEST_AFS, TEST_SWARM, TEST_QUEUE)
 
   requester.swarm.leave = leaveFake
 
@@ -102,7 +136,7 @@ test('requester.stop', async (t) => {
 })
 
 test('requester.prepareJob', async (t) => {
-  const requester = new Requester(jobNonce, matcher, user, afs, swarm, queue)
+  const requester = new Requester(TEST_JOB_NONCE, TEST_MATCHER, TEST_USER, TEST_AFS, TEST_SWARM, TEST_QUEUE)
 
   sinon.stub(rewards, 'getBudget').resolves(5)
   const submitFake = sinon.fake()
@@ -113,7 +147,7 @@ test('requester.prepareJob', async (t) => {
 })
 
 test('requester.onReceipt', async (t) => {
-  const requester = new Requester(jobNonce, matcher, user, afs, swarm, queue)
+  const requester = new Requester(TEST_JOB_NONCE, TEST_MATCHER, TEST_USER, TEST_AFS, TEST_SWARM, TEST_QUEUE)
 
   let onCompleteFake = false
   requester.receiptCountdown = new Countdown(1, () => { onCompleteFake = true })
@@ -132,7 +166,7 @@ test('requester.onHireConfirmed', async (t) => {
   sinon.stub(signature, 'getData').returns('data')
   sinon.stub(signature, 'getDid').returns('id1')
 
-  const requester = new Requester(jobNonce, matcher, user, afs, swarm, queue)
+  const requester = new Requester(TEST_JOB_NONCE, TEST_MATCHER, TEST_USER, TEST_AFS, TEST_SWARM, TEST_QUEUE)
 
   const quote = new Quote()
   quote.setSignature(signature)
@@ -164,7 +198,7 @@ test('requester.onHireConfirmed', async (t) => {
 })
 
 test('requester.sendRewards.valid', async (t) => {
-  const requester = new Requester(jobNonce, matcher, user, afs, swarm, queue)
+  const requester = new Requester(TEST_JOB_NONCE, TEST_MATCHER, TEST_USER, TEST_AFS, TEST_SWARM, TEST_QUEUE)
 
   const deliveryMap = new Map()
   deliveryMap.set('id1', 2)
@@ -176,7 +210,14 @@ test('requester.sendRewards.valid', async (t) => {
   const closeFake = sinon.fake()
   const connection = {
     sendReward: connectionFake,
-    close: closeFake
+    close: closeFake,
+    stream: {
+      on: () => true,
+      resume: () => true
+    },
+    onData: {
+      bind: () => true
+    }
   }
 
   const agreement1 = generateAgreement('id1', 2000)
@@ -198,7 +239,7 @@ test('requester.sendRewards.valid', async (t) => {
 })
 
 test('requester.sendRewards.none', async (t) => {
-  const requester = new Requester(jobNonce, matcher, user, afs, swarm, queue)
+  const requester = new Requester(TEST_JOB_NONCE, TEST_MATCHER, TEST_USER, TEST_AFS, TEST_SWARM, TEST_QUEUE)
 
   const connectionFake = sinon.fake()
   const closeFake = sinon.fake()
