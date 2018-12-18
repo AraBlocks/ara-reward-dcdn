88ee3df166511d0280f88d38864d4c76976da604
Mark Romano <markgromano31@gmail.com>
2018-12-18 13:13:58 -0500

    feat(autoqueue): resolved value from transaction

--- test/autoqueue.js
+++ test/autoqueue.js
@@ -6,6 +6,7 @@ test('autoqueue', async (t) => {
   const fake1 = sinon.fake()
   const fake2 = sinon.fake()
   const fake3 = sinon.fake()
+  const fake4 = sinon.fake.returns('foo')
   const fakeErr = sinon.fake()
 
   const queue = new AutoQueue()
@@ -17,9 +18,12 @@ test('autoqueue', async (t) => {
   })
   queue.push(fake2)
   await queue.push(fake3)
+  const foo = await queue.push(fake4)
 
   t.true(fake1.calledOnce)
   t.true(fake2.calledOnce)
   t.true(fake3.calledOnce)
+  t.true(fake4.calledOnce)
+  t.true('foo' === foo)
   t.true(fakeErr.calledOnce)
 })
