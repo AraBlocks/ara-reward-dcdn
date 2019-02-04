ecd42ef07fec2b7e6fe6e652209c32da6051a492
Madeline Cameron <github@maddie.today>
2019-02-04 17:09:18 -0500

    refactor(): Prepend debug with ara

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const extend = require('extend')
 const DCDN = require('./src/dcdn')
 const rc = require('ara-runtime-configuration')()
-const debug = require('debug')('ard')
+const debug = require('debug')('ara:ard')
 
 let instance = null
 
